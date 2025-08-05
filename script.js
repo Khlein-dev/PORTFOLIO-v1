97b7c3fef50202d65b122f1f4a4343f168d6002e
Khlein-dev <kierjerymiepascua@gmail.com>
2025-08-05 08:53:41 +0800

    button added

--- script.js
+++ script.js
@@ -1,25 +1,61 @@
-  
-  
 const buttons = document.querySelectorAll(".push");
 
 buttons.forEach(button => {
   button.addEventListener("click", function () {
     const targetClass = this.getAttribute("data-target");
     const targetDiv = document.querySelector(`.${targetClass}`);
+    const arrowImg = this.querySelector(".up img");
 
-    if (targetDiv) {
-      // Check if target is already visible
-      const isVisible = targetDiv.style.display === "block";
-
-      // Hide all containers first
-      document.querySelectorAll('.projcontainer').forEach(div => {
-        div.style.display = "none";
-      });
-
-      // Toggle the target
-      if (!isVisible) {
-        targetDiv.style.display = "block";
+    // Slide up all open containers and reset arrows
+    document.querySelectorAll(".projcontainer1, .projcontainer2, .projcontainer3, .projcontainer4").forEach(div => {
+      if (div !== targetDiv && div.style.display === "block") {
+        slideUp(div);
       }
+    });
+
+    document.querySelectorAll(".up img").forEach(img => {
+      if (img !== arrowImg) {
+        img.classList.remove("rotate180");
+      }
+    });
+
+    // Toggle current section
+    if (targetDiv.style.display === "block") {
+      slideUp(targetDiv);
+      arrowImg.classList.remove("rotate180");
+    } else {
+      slideDown(targetDiv);
+      arrowImg.classList.add("rotate180");
     }
   });
 });
+
+function slideDown(element) {
+  element.style.display = "block";
+  element.style.height = "0px";
+  element.style.overflow = "hidden";
+  element.style.transition = "height 0.5s ease";
+
+  requestAnimationFrame(() => {
+    element.style.height = "400px"; // match your CSS height
+  });
+
+  setTimeout(() => {
+    element.style.height = "400px";
+    element.style.overflow = "";
+    element.style.transition = "";
+  }, 500);
+}
+
+function slideUp(element) {
+  element.style.overflow = "hidden";
+  element.style.transition = "height 0.5s ease";
+  element.style.height = "0px";
+
+  setTimeout(() => {
+    element.style.display = "none";
+    element.style.height = "400px";
+    element.style.overflow = "";
+    element.style.transition = "";
+  }, 500);
+}
